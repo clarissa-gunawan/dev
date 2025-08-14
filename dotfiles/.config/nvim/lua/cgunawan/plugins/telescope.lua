8ba32df47f2701bb76ecce1114b2d07d81fdc92f
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2025-08-13 20:50:02 -0700

    feat: change telescope keybindings

--- dotfiles/.config/nvim/lua/cgunawan/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/cgunawan/plugins/telescope.lua
@@ -9,24 +9,30 @@ return {
 		require("telescope").setup({})
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {
 			desc = "Launch telescope with all files",
 		})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {
+		vim.keymap.set("n", "<leader>fp", builtin.git_files, {
 			desc = "Launch telescope with only git files",
 		})
-		vim.keymap.set("n", "<leader>pws", function()
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+			desc = "Launch telescope with live grep",
+		})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+			desc = "Launch telescope with buffers",
+		})
+		vim.keymap.set("n", "<leader>fws", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
 		end)
-		vim.keymap.set("n", "<leader>pWs", function()
+		vim.keymap.set("n", "<leader>fWs", function()
 			local word = vim.fn.expand("<cWORD>")
 			builtin.grep_string({ search = word })
 		end)
-		vim.keymap.set("n", "<leader>ps", function()
+		vim.keymap.set("n", "<leader>fs", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end, { desc = "Launch telescope with grep as filter" })
-		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 	end,
 }
 
