6acc6514c6cdfe9b897cc9fa6a37d5294541e899
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2025-08-24 00:44:06 -0700

    refactor: cht sh to handle more languages and core utils

--- dotfiles/.local/scripts/cht.sh
+++ dotfiles/.local/scripts/cht.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-languages=$(echo "python3 bash c cpp cmake javascript typescript golang rust django" | tr " " "\n")
-core_utils=$(echo "find xargs sed awk grep docker rg fd tar zip ssh ssh-keygen tmux vim nvim :help :intro" | tr " " "\n")
+languages=(
+    "python3" "bash" "c" "cpp" "cmake" "javascript" "typescript"
+    "golang" "rust" "django"
+)
+core_utils=(
+    "find" "xargs" "sed" "awk" "grep" "docker" "rg" "fd" "bc" "git"
+    "sort" "uniq" "jq" "tr" "parallel" "tar" "zip" "ssh" "ssh-keygen"
+    "tmux" "vim" "nvim" "head" "tail" ":help" ":intro"
+)
+
+# Convert to newline-separated strings for fzf
+languages=$(printf '%s\n' "${languages[@]}")
+core_utils=$(printf '%s\n' "${core_utils[@]}")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
 
 read -p "Query (try :learn, or :list for languages or empty for core utilies): " query
