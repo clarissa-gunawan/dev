6b21e1b16fc1a92ffbe21d2e23aacf79840d8921
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2025-09-04 09:37:41 -0700

    feat: set conform for xml improvement

--- dotfiles/.config/nvim/lua/cgunawan/lazy/conform.lua
+++ dotfiles/.config/nvim/lua/cgunawan/lazy/conform.lua
@@ -15,33 +15,32 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = "fallback",
-          }
-        end
-      end,
+      format_on_save = {
+        pattern = "*.{lua,python,yaml,json,md,sh,bash,c,cpp,rust,go,js,ts,html,css,scss,xml,cmake,ansible}",
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
         python = { "ruff_organize_imports", "ruff_fix", "ruff_format" },
+        c = { "clang-format" },
         cpp = { "clang-format" },
-        xml = { "xmllint", "xmlformatter", "xmlstarlet", stop_after_first = true },
-        ansible = { "ansible-lint" },
-        cmake = { "cmake_format" },
+        rust = { "rustfmt" },
+        go = { "gofumpt" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        html = { "prettierd", "prettier", stop_after_first = true },
+        css = { "prettierd", "prettier", stop_after_first = true },
+        scss = { "prettierd", "prettier", stop_after_first = true },
+        xml = { "prettierd", "prettier", stop_after_first = true },
+        ansible = { "prettierd", "prettier" },
+        cmake = { "prettierd", "prettier" },
         yaml = { "yamlfmt", "yamlfix", stop_after_first = true },
         json = { "prettierd", "prettier", stop_after_first = true },
         markdown = { "prettierd", "prettier", stop_after_first = true },
-        sh = { "shfmt", "shellcheck" },
-        bash = { "shfmt", "shellcheck" },
+        sh = { "shfmt" },
+        bash = { "shfmt" },
         -- Apply to all files as fallback
         ["_"] = { "trim_whitespace", "trim_newlines" },
         -- You can use 'stop_after_first' to run the first available formatter from the list
