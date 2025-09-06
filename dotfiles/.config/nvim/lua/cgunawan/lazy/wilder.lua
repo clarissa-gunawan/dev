b07ff5f70b04afc5ed2a2feecd3219a95fbdc92b
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2025-09-06 14:30:02 -0700

    fix: wilder use lua fzy filter for / and ? and use vim fuzzy filter for :

--- dotfiles/.config/nvim/lua/cgunawan/lazy/wilder.lua
+++ dotfiles/.config/nvim/lua/cgunawan/lazy/wilder.lua
@@ -2,7 +2,24 @@ return {
   {
     "gelguy/wilder.nvim",
     config = function()
-      require("wilder").setup { modes = { ":", "/", "?" } }
+      local wilder = require "wilder"
+      wilder.setup { modes = { ":", "/", "?" } }
+
+      wilder.set_option("pipeline", {
+        wilder.branch(
+          -- For command line (:) - use basic filter
+          wilder.cmdline_pipeline {
+            fuzzy = 1,
+            -- Use basic Vim filter instead of lua_fzy
+            fuzzy_filter = wilder.vim_fuzzy_filter(),
+          },
+          -- For search (/ and ?) - use lua_fzy which works
+          wilder.vim_search_pipeline {
+            fuzzy = 1,
+            fuzzy_filter = wilder.lua_fzy_filter(),
+          }
+        ),
+      })
     end,
   },
 }
