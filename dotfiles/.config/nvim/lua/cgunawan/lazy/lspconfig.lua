a346366351a5edab7eef24448599b8de99277162
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2025-11-08 02:21:04 +0000

    chore: remove devcontianer cli

--- dotfiles/.config/nvim/lua/cgunawan/lazy/lspconfig.lua
+++ dotfiles/.config/nvim/lua/cgunawan/lazy/lspconfig.lua
@@ -13,32 +13,6 @@ return {
     },
   },
   {
-    "arnaupv/nvim-devcontainer-cli",
-    opts = {
-      -- By default, if no extra config is added, following nvim_dotfiles are
-      -- installed: "https://github.com/LazyVim/starter"
-      -- This is an example for configuring other nvim_dotfiles inside the docker container
-      nvim_dotfiles_repo = "https://github.com/clarissa-gunawan/dev.git",
-      nvim_dotfiles_install_command = "rm -rf $HOME/.config/nvim && cp dotfiles/.config/nvim $HOME/.config",
-      -- In case you want to change the way the devenvironment is setup, you can also provide your own setup
-      setup_environment_repo = "https://github.com/clarissa-gunawan/dev.git",
-      setup_environment_install_command = "./run nvim",
-    },
-    keys = {
-    -- stylua: ignore
-    {
-      "<leader>cdu",
-      ":DevcontainerUp<cr>",
-      desc = "Up the DevContainer",
-    },
-      {
-        "<leader>cdc",
-        ":DevcontainerConnect<cr>",
-        desc = "Connect to DevContainer",
-      },
-    },
-  },
-  {
     -- Main LSP Configuration
     "neovim/nvim-lspconfig",
     dependencies = {
