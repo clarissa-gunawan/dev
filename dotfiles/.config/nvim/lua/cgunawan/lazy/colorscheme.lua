1966cb109a9f2be9a0f80d29c8423330d6cd2f96
Clarissa Gunawan <gunawan.clarissa@outlook.com>
2026-01-17 13:45:44 -0800

    chore: change nvim theme to tokyonight-night

--- dotfiles/.config/nvim/lua/cgunawan/lazy/colorscheme.lua
+++ dotfiles/.config/nvim/lua/cgunawan/lazy/colorscheme.lua
@@ -28,7 +28,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
@@ -69,7 +69,7 @@ return {
     -- https://vimcolorschemes.com/cocopon/iceberg.vim
     "cocopon/iceberg.vim",
     config = function()
-      vim.cmd.colorscheme "iceberg"
+      -- vim.cmd.colorscheme "iceberg"
       vim.o.background = "dark"
     end,
   },
